d71996dce80a94f0dfe6becdaaeedaa9c11eb2f4
Nes224 <Nes224@hotmail.com>
2024-08-07 12:06:52 +0700

    first commit

---
+++ pytorch_tutorial.py
@@ -0,0 +1,18 @@
+import torch
+
+# x = torch.empty(3) #(n) d vector with (n) elements.
+# x = torch.empty(2,2,2)
+# x = torch.rand(2,2)
+# x = torch.zeros(2,2)
+# x = torch.ones(2,2, dtype=torch.float16)
+# x = torch.tensor([2.5,0.1])
+# print(x.dtype)
+# print(x.size())
+# print(x)
+
+x = torch.rand(2,2)
+y = torch.rand(2,2)
+print(x)
+print(y)
+z = x + y
+print(z)
