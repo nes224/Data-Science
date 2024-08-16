b4cea8cd98af73e74d84f33ef916585e05020642
Nes224 <Nes224@hotmail.com>
2024-08-16 11:04:41 +0700

    deep learning

--- pytorch_tutorial/15_tensorboard.py
+++ pytorch_tutorial/15_tensorboard.py
@@ -1,16 +1,17 @@
-# MNIST
-# DataLoader, Transformation
-# Multilayer Neural Net, activation function
-# Loss and Optimizer
-# Training Loop (batch training)
-# Model evaluation
-# GPU support
 
 import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
+
+############## TENSORBOARD ########################
+import sys
+import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
+# default `log_dir` is "runs" - we'll be more specific here
+writer = SummaryWriter('runs/mnist1')
+###################################################
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -19,8 +20,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 input_size = 784 # 28x28
 hidden_size = 500 
 num_classes = 10
-num_epochs = 2
-batch_size = 100
+num_epochs = 1
+batch_size = 64
 learning_rate = 0.001
 
 # MNIST dataset 
@@ -48,7 +49,14 @@ example_data, example_targets = next(examples)
 for i in range(6):
     plt.subplot(2,3,i+1)
     plt.imshow(example_data[i][0], cmap='gray')
-plt.show()
+#plt.show()
+
+############## TENSORBOARD ########################
+img_grid = torchvision.utils.make_grid(example_data)
+writer.add_image('mnist_images', img_grid)
+#writer.close()
+#sys.exit()
+###################################################
 
 # Fully connected neural network with one hidden layer
 class NeuralNet(nn.Module):
@@ -72,7 +80,15 @@ model = NeuralNet(input_size, hidden_size, num_classes).to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
 
+############## TENSORBOARD ########################
+writer.add_graph(model, example_data.reshape(-1, 28*28).to(device))
+#writer.close()
+#sys.exit()
+###################################################
+
 # Train the model
+running_loss = 0.0
+running_correct = 0
 n_total_steps = len(train_loader)
 for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader):  
@@ -90,11 +106,24 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
         
+        running_loss += loss.item()
+
+        _, predicted = torch.max(outputs.data, 1)
+        running_correct += (predicted == labels).sum().item()
         if (i+1) % 100 == 0:
             print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
+            ############## TENSORBOARD ########################
+            writer.add_scalar('training loss', running_loss / 100, epoch * n_total_steps + i)
+            running_accuracy = running_correct / 100 / predicted.size(0)
+            writer.add_scalar('accuracy', running_accuracy, epoch * n_total_steps + i)
+            running_correct = 0
+            running_loss = 0.0
+            ###################################################
 
 # Test the model
 # In test phase, we don't need to compute gradients (for memory efficiency)
+class_labels = []
+class_preds = []
 with torch.no_grad():
     n_correct = 0
     n_samples = 0
@@ -103,9 +132,29 @@ with torch.no_grad():
         labels = labels.to(device)
         outputs = model(images)
         # max returns (value ,index)
-        _, predicted = torch.max(outputs.data, 1)
+        values, predicted = torch.max(outputs.data, 1)
         n_samples += labels.size(0)
         n_correct += (predicted == labels).sum().item()
 
+        class_probs_batch = [F.softmax(output, dim=0) for output in outputs]
+
+        class_preds.append(class_probs_batch)
+        class_labels.append(labels)
+
+    # 10000, 10, and 10000, 1
+    # stack concatenates tensors along a new dimension
+    # cat concatenates tensors in the given dimension
+    class_preds = torch.cat([torch.stack(batch) for batch in class_preds])
+    class_labels = torch.cat(class_labels)
+
     acc = 100.0 * n_correct / n_samples
     print(f'Accuracy of the network on the 10000 test images: {acc} %')
+
+    ############## TENSORBOARD ########################
+    classes = range(10)
+    for i in classes:
+        labels_i = class_labels == i
+        preds_i = class_preds[:, i]
+        writer.add_pr_curve(str(i), labels_i, preds_i, global_step=0)
+        writer.close()
+    ###################################################
